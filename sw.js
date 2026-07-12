8d9c878d006b0ea1d9ed434b2e90226c7f0192f1
Cassey88 <kcchan.nz@gmail.com>
2026-07-12 17:34:27 +1200

    Switch to local-file variant: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fs-family-outing-v5';
+const CACHE = 'file-shortcut-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
